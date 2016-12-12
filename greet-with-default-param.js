b93f33b0917494e53d52284d506fc98b377c9b46
Robert Harężlak <harezlak.robert7@gmail.com>
2016-12-12 19:46:37 +0100

    exercises for typescript, auto compile to vanilla js

---
+++ greet-with-default-param.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Typical Juzer";
+document.body.innerHTML = greeter(user);
---
+++ greet-with-default-param.ts
@@ -0,0 +1,6 @@
+function greeter(person: string) {
+    return "Hello, " + person;
+}
+var user = "Typical Juzer";
+
+document.body.innerHTML = greeter(user);
